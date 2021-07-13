a1a29a7695924407ac2498a1b92d04002cbb08de
nateos <sodjanathan@gmail.com>
2021-07-13 10:47:54 +0000

    setting up additional files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './styles/App.scss';
+import './styles/bootstrap5.min.css';
+import data from './data.json'
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>React Bootstrap</h1>
       </header>
     </div>
   );
