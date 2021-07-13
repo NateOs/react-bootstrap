85f49e17dcc938ae295f5ccd85dcc4d0a3747cec
nateos <sodjanathan@gmail.com>
2021-07-13 13:24:54 +0000

    calling components, importing useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
-import './styles/App.scss';
-import './styles/bootstrap5.min.css';
-import data from './data.json'
-
+import { React, useState } from "react";
+import "./styles/App.scss";
+import "./styles/bootstrap5.min.css";
+import { Container, Row, Col } from "react-bootstrap";
+import PizzaCard from "./components/PizzaCard";
+import Confirmation from "./components/Confirmation";
+import data from "./data.json";
 
 function App() {
+  const [ordered, setOrdered] = useState(false);
+
+  const displayInfo = () => {
+    setOrdered(true);
+    setTimeout(() => setOrdered("false"), 3000);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React Bootstrap</h1>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Row className="d-flex flex-row">
+            <PizzaCard />
+        </Row>
+      </Container>
+    </>
   );
 }
 
