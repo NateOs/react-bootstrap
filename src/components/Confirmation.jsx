9495fa22e452872801d63a20890fbfc7c3ddc199
nateos <sodjanathan@gmail.com>
2021-07-13 14:57:53 +0000

    bugfix

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,16 +1,14 @@
-import React from "react";
-import { Toast } from 'react-bootstrap';
+import { React} from "react";
+import { Toast } from "react-bootstrap";
 
 export default function Confirmation({ toggle }) {
   return (
-    <Toast onClose={() => toggle(false)}>
+    <Toast onClose={toggle}>
       <Toast.Header>
-        <strong className="mr-auto">Your order is in the oven</strong> 
-        <small>Just now</small> 
+        <strong className="mr-auto">Your order is in the oven</strong>
+        <small>Just now</small>
       </Toast.Header>
-      <Toast.Body>
-        Your order is on the way
-      </Toast.Body>
+      <Toast.Body>Your order is on the way</Toast.Body>
     </Toast>
-  )
+  );
 }
